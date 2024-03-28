9a13565731195f88ad2facd92d242ac60a05deb9
Uzair16ahmed <khanuzair285@gmail.com>
2024-03-28 14:28:51 +0500

    product div created  for every product in product wrapper

--- script.js
+++ script.js
@@ -1,74 +1,116 @@
 const products = [
     {
       name: 'Sony Playstation 5',
-      url: '/images/playstation-5.png',
+      url: 'Images/playstation_5.png',
       category: 'games',
       price: 499.99,
     },
     {
       name: 'Samsung Galaxy',
-      url: '/images/samsung-galaxy.png',
+      url: 'Images/samsung_galaxy.png',
       category: 'smartphones',
       price: 399.99,
     },
     {
       name: 'Cannon EOS Camera',
-      url: '/images/cannon-eos-camera.png',
+      url: 'Images/cannon_eos_camera.png',
       category: 'cameras',
       price: 749.99,
     },
     {
       name: 'Sony A7 Camera',
-      url: '/images/sony-a7-camera.png',
+      url: 'Images/sony_a7_camera.png',
       category: 'cameras',
       price: 1999.99,
     },
     {
       name: 'LG TV',
-      url: '/images/lg-tv.png',
+      url: 'Images/lg_tv.png',
       category: 'televisions',
       price: 799.99,
     },
     {
       name: 'Nintendo Switch',
-      url: '/images/nintendo-switch.png',
+      url: 'Images/nintendo_switch.png',
       category: 'games',
       price: 299.99,
     },
     {
       name: 'Xbox Series X',
-      url: '/images/xbox-series-x.png',
+      url: 'Images/xbox_series_x.png',
       category: 'games',
       price: 499.99,
     },
     {
       name: 'Samsung TV',
-      url: '/images/samsung-tv.png',
+      url: 'Images/samsung_tv.png',
       category: 'televisions',
       price: 1099.99,
     },
     {
       name: 'Google Pixel',
-      url: '/images/google-pixel.png',
+      url: 'Images/google_pixel.png',
       category: 'smartphones',
       price: 499.99,
     },
     {
       name: 'Sony ZV1F Camera',
-      url: '/images/sony-zv1f-camera.png',
+      url: 'Images/sony_zv1f_camera.png',
       category: 'cameras',
       price: 799.99,
     },
     {
       name: 'Toshiba TV',
-      url: '/images/toshiba-tv.png',
+      url: 'Images/toshiba_tv.png',
       category: 'televisions',
       price: 499.99,
     },
     {
       name: 'iPhone 14',
-      url: '/images/iphone-14.png',
+      url: 'Images/iphone_14.png',
       category: 'smartphones',
       price: 999.99,
     },
   ];
+
+
+  // Select DOM Elements
+
+  const productsWrapper = document.getElementById('products-wrapper');
+  const checkboxes = document.querySelectorAll('check');
+  const filtersContainer = document.getElementById('filters-container');
+  const searchInput = document.getElementById('search');
+  const cartCount = document.getElementById('cart-count');
+
+
+  // Init cart item count
+
+  let cartItemCount = 0;
+  
+  // Init product element array
+  const productsElements = [];
+
+  // Loop over products and create an element
+  products.forEach((product) => {
+    const productElement = document.createElement('div');
+
+    productElement.className = 'item space-y-2';
+
+    productElement.innerHTML = `
+                <div class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl">
+                    <img 
+                        src="${product.url}"
+                        alt="${product.name}"
+                        class="w-full h-full object-cover"
+                    />
+                    <button class="status bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0">
+                        Add to Cart
+                    </button>
+                </div>
+                <p class="text-xl">${product.name}</p>
+                <strong>${product.price.toLocaleString()}</strong>
+                `;
+
+    productsElements.push(productElement);
+    productsWrapper.appendChild(productElement)
+});
